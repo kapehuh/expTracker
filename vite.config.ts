e7fbf9a25577d8b3123fb886a3289f56b1b5041a
Щегольков С.Д. <shchegolkov_sd@lgnch.spb.ru>
2026-03-19 11:38:08 +0300

    test+lint12.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/expTracker/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
