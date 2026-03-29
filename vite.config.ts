01b48e633929372f0d4d220f64f325e544ce5ede
Щегольков С.Д. <shchegolkov_sd@lgnch.spb.ru>
2026-03-29 18:24:07 +0300

    fv5

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/expTracker/',
+  base: process.env.NODE_ENV === 'production' ? '/expTracker/' : '/',
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
