406a2516ed6c0c649a2137892e8ef8d8989c6b93
Щегольков С.Д. <shchegolkov_sd@lgnch.spb.ru>
2026-03-29 17:41:12 +0300

    fv3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './hooks/useAuth';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
@@ -9,7 +9,7 @@ function App() {
   const { currentUser } = useAuth();
 
   return (
-    <HashRouter basename="/expTracker">
+    <BrowserRouter basename="/expTracker">
       <Routes>
         {/* Если пользователь уже залогинен, с главной (/login) отправляем на /dashboard */}
         <Route
@@ -36,7 +36,7 @@ function App() {
           }
         />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
